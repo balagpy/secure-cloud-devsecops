2c766b4ed3671322e484a2932312ce993af0951a
Balaji <balagpy@gmail.com>
2026-05-24 14:08:16 +0530

    Create secrets.js

---
+++ secrets.js
@@ -0,0 +1,9 @@
+module.exports = {
+  AWS_ACCESS_KEY_ID: "AKIAIOSFODNN7EXAMPLE",
+  AWS_SECRET_ACCESS_KEY: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+  GITHUB_TOKEN: "ghp_1234567890abcdefghijklmnopqrstuvwxyz",
+  SLACK_WEBHOOK: "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX",
+  DATABASE_URL: "postgres://admin:password123@localhost:5432/prod",
+  JWT_SECRET: "my-hardcoded-jwt-secret",
+  STRIPE_SECRET_KEY: "sk_live_51ExampleSecretKey"
+};
